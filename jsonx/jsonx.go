299ce70c02d9ebfaf1cd36ea3806eacf4958d8d4
passer <whzhoua@gmail.com>
2023-11-05 00:05:19 +0800

    feat: add jsonx

--- jsonx/jsonx.go
+++ jsonx/jsonx.go
@@ -1,1 +1,61 @@
 package jsonx
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+)
+
+func MustToJSON(v interface{}) string {
+	if value, err := ToJSON(v); err != nil {
+		panic(err)
+	} else {
+		return value
+	}
+}
+
+func ToFormatJSON(v interface{}, indent int) (string, error) {
+	bs, err := json.MarshalIndent(v, "", strings.Repeat(" ", indent))
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func ToJSON(v interface{}) (string, error) {
+	bs, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func MustJSONToAny(s string, a interface{}) error {
+	if err := JSONToAny(s, a); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func JSONToAny(s string, a interface{}) error {
+	d := json.NewDecoder(bytes.NewReader([]byte(s)))
+	d.UseNumber()
+
+	return d.Decode(a)
+}
+
+func FormatJSON(s string, indent int) (string, error) {
+	var a interface{}
+	err := JSONToAny(s, &a)
+	if err != nil {
+		return "", err
+	}
+
+	formatJSON, err := ToFormatJSON(a, indent)
+	if err != nil {
+		return "", err
+	}
+	return formatJSON, nil
+}
--- jsonx/jsonx_test.go
+++ jsonx/jsonx_test.go
@@ -1,1 +1,19 @@
 package jsonx
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFormatJSON(t *testing.T) {
+	a := assert.New(t)
+
+	content := `["123", "456"]`
+	expectContent := `[
+    "123",
+    "456"
+]`
+	value, err := FormatJSON(content, 4)
+	a.NoError(err)
+	a.Equal(value, expectContent)
+}
