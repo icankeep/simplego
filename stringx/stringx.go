dc59d2b28b190a9eb552c3cd599f91ac3e53150f
passer <whzhoua@gmail.com>
2023-11-05 16:20:06 +0800

    feat: add stringx

--- stringx/stringx.go
+++ stringx/stringx.go
@@ -1,1 +1,39 @@
 package stringx
+
+import "unicode"
+
+func IsEmpty(s string) bool {
+	return len(s) == 0
+}
+
+func IsNotEmpty(s string) bool {
+	return !IsEmpty(s)
+}
+
+func IsNotBlank(s string) bool {
+	return !IsBlank(s)
+}
+
+func IsBlank(s string) bool {
+	for _, r := range s {
+		if !unicode.IsSpace(r) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsNumeric Checks if the CharSequence contains only Unicode digits. A decimal point is not a Unicode digit and returns false.
+// null will return false. An empty CharSequence (len=0) will return false.
+// Note that the method does not allow for a leading sign, either positive or negative
+func IsNumeric(s string) bool {
+	if IsEmpty(s) {
+		return false
+	}
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
+}
--- stringx/stringx_test.go
+++ stringx/stringx_test.go
@@ -1,1 +1,70 @@
 package stringx
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_IsEmpty(t *testing.T) {
+	a := assert.New(t)
+
+	a.True(IsEmpty(""))
+	a.False(IsEmpty("TEST"))
+	a.False(IsEmpty(" "))
+	a.False(IsEmpty("\t"))
+	a.False(IsEmpty("\n"))
+	a.False(IsEmpty("꯱"))
+}
+
+func Test_IsNotEmpty(t *testing.T) {
+	a := assert.New(t)
+
+	a.False(IsNotEmpty(""))
+	a.True(IsNotEmpty("TEST"))
+	a.True(IsNotEmpty(" "))
+	a.True(IsNotEmpty("\t"))
+	a.True(IsNotEmpty("\n"))
+	a.True(IsNotEmpty("꯱"))
+}
+
+func Test_IsBlank(t *testing.T) {
+	a := assert.New(t)
+
+	a.True(IsBlank(""))
+	a.True(IsBlank(" "))
+	a.True(IsBlank("\t"))
+	a.True(IsBlank("\n"))
+	a.True(IsBlank("\n  \t"))
+	a.True(IsBlank("\n  "))
+	a.True(IsBlank("  \t  "))
+	a.True(IsBlank("     "))
+	a.False(IsBlank("  s   "))
+	a.False(IsBlank("xxx"))
+}
+
+func Test_IsNotBlank(t *testing.T) {
+	a := assert.New(t)
+
+	a.False(IsNotBlank(""))
+	a.False(IsNotBlank(" "))
+	a.False(IsNotBlank("\t"))
+	a.False(IsNotBlank("\n"))
+	a.False(IsNotBlank("\n  \t"))
+	a.False(IsNotBlank("\n  "))
+	a.False(IsNotBlank("  \t  "))
+	a.False(IsNotBlank("     "))
+	a.True(IsNotBlank("  s   "))
+	a.True(IsNotBlank("xxx"))
+}
+
+func Test_IsNumeric(t *testing.T) {
+	a := assert.New(t)
+
+	a.True(IsNumeric("0"))
+	a.True(IsNumeric("2"))
+	a.True(IsNumeric("9"))
+	a.False(IsNumeric("X"))
+	a.False(IsNumeric("1x"))
+	a.False(IsNumeric("0x1234"))
+	a.False(IsNumeric(" "))
+}
